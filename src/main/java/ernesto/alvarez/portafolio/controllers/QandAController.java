377e775879dd36e354450d3009ed788e5b450ea8
Ernesto Alvarez <43563276+IngErnestoAlvarez@users.noreply.github.com>
2021-08-02 13:59:28 -0300

    Maked template for qanda

--- src/main/java/ernesto/alvarez/portafolio/controllers/QandAController.java
+++ src/main/java/ernesto/alvarez/portafolio/controllers/QandAController.java
@@ -1,7 +1,12 @@
 package ernesto.alvarez.portafolio.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import ernesto.alvarez.portafolio.model.qanda.GameFacade;
 
 /**
  * QandAController
@@ -9,8 +14,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class QandAController {
 
+    @Autowired
+    GameFacade qanda;
+
     @GetMapping("/game")
-    public String game() {
+    public String game(@RequestParam String game, Model model) {
+        model.addAttribute("game", game);
+        model.addAttribute("question", qanda.getActualQuestion());
+        model.addAttribute("answer1", qanda.getAnswer(0));
         return "qanda";
     }
 }
