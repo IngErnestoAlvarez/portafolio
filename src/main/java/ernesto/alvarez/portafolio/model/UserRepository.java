7f4791f8cc3ce18b7b311f638b61a999f8b7c575
Ernesto Alvarez <43563276+IngErnestoAlvarez@users.noreply.github.com>
2021-08-02 18:51:52 -0300

    Success on connection to database

---
+++ src/main/java/ernesto/alvarez/portafolio/model/UserRepository.java
@@ -0,0 +1,7 @@
+package ernesto.alvarez.portafolio.model;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface UserRepository extends CrudRepository<UserJDA, Integer> {
+
+}
